cb42dbec1f806a4c3b0664a79ad6533e67e28cdd
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-12-17 00:33:44 +0530

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -34,22 +34,6 @@ targets = got_data['Assists']
 weights = got_data['xG']
 edge_data = zip(sources, targets, weights)
 
-for e in edge_data:
-    src = e[0]
-    dst = e[1]
-    w = e[2]
-    
-    
-    got_net.add_node(src, src, title=src)
-    got_net.add_node(dst, dst, title=dst)
-    got_net.add_edge(src, dst, value=w)
-    
-
-neighbor_map = got_net.get_adj_list()
-for node in got_net.nodes:
-                node["title"] += " Neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
-                node["value"] = len(neighbor_map[node["id"]])
-
 
 
 path = '/tmp'
