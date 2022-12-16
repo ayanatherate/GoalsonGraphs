c3a9cca2933f7d8c167510dcaffe812ad627c804
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-12-17 00:32:49 +0530

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -13,13 +13,20 @@ import networkx as nx
 from pyvis.network import Network
 
 df=pd.read_csv(r"https://raw.githubusercontent.com/ayanatherate/WC2022finals.github.io/main/Argentina_WC2022.csv")
-st.write(df)
+
+G = nx.from_pandas_edgelist(df, 'Player', 'Assists', 'xG')
 got_net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
 
 
 # set the physics layout of the network
-got_net.barnes_hut()
+got_net.from_nx(G)
 
+    # Generate network with specific layout settings
+got_net.repulsion(node_distance=420,
+                   central_gravity=0.33,
+                   spring_length=110,
+                   spring_strength=0.10,
+                   damping=0.95)
 got_data = df
 
 sources = got_data['Player']
