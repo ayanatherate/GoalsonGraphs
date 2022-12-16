d12feeb6a572199475daad9470699d5cd6671148
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-12-17 00:02:28 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -6,4 +6,4 @@ from pyvis.network import Network
 
 HtmlFile = open(f'index.html','r',encoding='utf-8')
 
-components.html(HtmlFile.read(),width=600)
+components.html(HtmlFile.read(),width=600,height=1000)
