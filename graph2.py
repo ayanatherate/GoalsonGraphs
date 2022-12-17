9fe7b0f279eed30fe9cb8ddd1bba1daa32f6c9d9
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-12-17 12:16:57 +0530

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -43,16 +43,19 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 title_html=f'<h1 style="font-family:Calibri; color:#DEF294; font-size: 30px;">Graph Network Analysis of WC 2022 finalists, all Goals and Chances created : Argentina & France</h1>'
 st.markdown(title_html, unsafe_allow_html=True)
 st.write()
+
 caption1='Why this Project?'
 write1='1) Visualization is cool.'
 write2='2) (1) again but let me explain a bit in detail. The following Network graphs tries to visualize the relationships and the consecutive linkages between the players of each team, as an average of all Goal Chances (Shots/Goals) created during their World Cup journey, so far.(up untill the Final on Sunday).'
-writeup1_html=f'<h3 style="font-family:Calibri; color:#DEF294; font-size: 10px;">{caption1}<br>{write1}<br>{write2}</h3>'
+writeup1_html=f'<h3 style="font-family:Calibri; color:#DBEDEE; font-size: 15px;">{caption1}<br><br>{write1}<br>{write2}</h3>'
 st.markdown(writeup1_html, unsafe_allow_html=True)
-st.write()
-st.caption('How to view these Graphs.')
-st.write('The Graphs are interactive. So you can click on any bubble inside the graphs to view the nearest neighbors associated with each player. For example, Lionel Messi has Dybala, Otamendi, Di Maria & others as neighbors in the graph. That means, everytime a Goal Chance was created by Messi, he was linked with the players that are in his nwighbourhood.')
-st.write('It goes without saying that Players (nodes) in the graph with the maximum number of edges connected to it have been the players with the most number of Goal Chances created. More often than not, they are much centrallized in the Graph Structure.')
 
+st.write()
+caption2='How to view these Graphs.'
+write3='The Graphs are interactive. So you can click on any bubble inside the graphs to view the nearest neighbors associated with each player. For example, Lionel Messi has Dybala, Otamendi, Di Maria & others as neighbors in the graph. That means, everytime a Goal Chance was created by Messi, he was linked with the players that are in his nwighbourhood.'
+write4='It goes without saying that Players (nodes) in the graph with the maximum number of edges connected to it have been the players with the most number of Goal Chances created. More often than not, they are much centrallized in the Graph Structure.'
+writeup2_html=f'<h3 style="font-family:Calibri; color:#DBEDEE; font-size: 15px;">{caption2}<br><br>{write3}<br>{write4}</h3>'
+st.markdown(writeup2_html, unsafe_allow_html=True)
 st.write()
 
 
