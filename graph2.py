4280e27c8b1f69947088e7470c180b56b5c8fa21
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-12-17 13:22:52 +0530

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -46,14 +46,14 @@ st.write()
 
 caption1='Why this Project?'
 write1='1) Visualizing Data is cool.'
-write2='2) (1) again but let me explain a bit in detail. The following Network graphs tries to visualize the relationships and the consecutive linkages between the players of each team, as an average of all Goal Chances (Shots/Shots on Goal/Goals) created during their World Cup journey, so far.(up untill the Final on Sunday).'
+write2='2) (1) again but let me explain a bit in detail. The following Network graphs tries to visualize the relationships and the consecutive linkages between the players of each team, as an accumulation of all Goal Chances (Shots/Shots on Goal/Goals) created during their World Cup journey, so far.(up untill the Final on Sunday).'
 writeup1_html=f'<h3 style="font-family:Calibri; color:#DBEDEE; font-size: 15px;">{caption1}<br><br>{write1}<br>{write2}</h3>'
 st.markdown(writeup1_html, unsafe_allow_html=True)
 
 st.write()
 caption2='What exactly are these graph networks?'
 write3='The Graphs are interactive. So you can click on any bubble inside the graphs to view the nearest neighbors associated with each player. For example, Lionel Messi has Dybala, Otamendi, Di Maria & others as neighbors in the graph. That means, everytime a Goal Chance was created by Messi, he was linked with either or more of the players that are in his neighbourhood.'
-write4='It goes without saying that Players (nodes) in the graph with the maximum number of edges connected to it have been the players with the most number of Goal Chances created. More often than not, they are much centrallized in the Graph Structure.'
+write4='It goes without saying that Players (nodes) in the graph with the maximum number of edges connected to it have been the players with the most number of Goal Chances & Goals created. More often than not, they are much centrallized in the Graph Structure.'
 writeup2_html=f'<h3 style="font-family:Calibri; color:#DBEDEE; font-size: 15px;">{caption2}<br><br>{write3}<br>{write4}</h3>'
 st.markdown(writeup2_html, unsafe_allow_html=True)
 st.write()
