1d3033e5eb959ba1c53e3b99c86538259462327b
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-12-18 10:32:07 +0530

    Delete graph.py

--- graph.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-import pandas as pd
-import networkx as nx
-from pyvis.network import Network
-
-HtmlFile = open(f'index.html','r')
-
-components.html(HtmlFile.read(),width=600,height=1000)
